3b9f90eece919acc902ed67701da3e6fdd33ed8c
GiovanniKarra <karragiovanni@gmail.com>
2022-08-13 16:52:41 +0200

    rr

--- Assets/scripts/Rope.cs
+++ Assets/scripts/Rope.cs
@@ -8,15 +8,27 @@ public class Rope : MonoBehaviour
     Transform dog;
     Transform playerHand;
 
+    int playerSortingOrder;
+
     private void Awake()
     {
         rope = GetComponent<LineRenderer>();
         dog = FindObjectOfType<Dog>().transform;
         playerHand = GameObject.Find("Hand").transform;
+        playerSortingOrder = playerHand.GetComponentInParent<SpriteRenderer>().sortingOrder;
     }
 
     private void Update()
     {
+        if (dog.position.y < playerHand.position.y)
+        {
+            rope.sortingLayerName = "Default";
+            rope.sortingOrder = playerSortingOrder + 1;
+        }
+        else
+        {
+            rope.sortingLayerName = "Under";
+        }
         rope.SetPosition(0, playerHand.position); rope.SetPosition(1, dog.position);
     }
 }
